591622fb1313da103c79adabbd9267d1029872a9
VlV <92279938+VlV-515@users.noreply.github.com>
2024-10-22 22:55:40 -0600

    Feat⚙️Add firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,24 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'finanzas-vlv',
+        appId: '1:520471745398:web:b45d6db7915f7e2e24e057',
+        storageBucket: 'finanzas-vlv.appspot.com',
+        apiKey: 'AIzaSyAhmdd4YuHRtqqZuJ3oG0rcY0DNzDbAGyk',
+        authDomain: 'finanzas-vlv.firebaseapp.com',
+        messagingSenderId: '520471745398',
+      })
+    ),
+    provideFirestore(() => getFirestore()),
+  ],
 };
