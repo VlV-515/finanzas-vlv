c6f6356e418ce5bb73469e611e576ce57c02a58f
VlV <92279938+VlV-515@users.noreply.github.com>
2024-10-22 22:58:09 -0600

    Refactor🔨Migrate firebase config to env

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,22 +5,14 @@ import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { environment } from '@env/environment.development';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync(),
-    provideFirebaseApp(() =>
-      initializeApp({
-        projectId: 'finanzas-vlv',
-        appId: '1:520471745398:web:b45d6db7915f7e2e24e057',
-        storageBucket: 'finanzas-vlv.appspot.com',
-        apiKey: 'AIzaSyAhmdd4YuHRtqqZuJ3oG0rcY0DNzDbAGyk',
-        authDomain: 'finanzas-vlv.firebaseapp.com',
-        messagingSenderId: '520471745398',
-      })
-    ),
+    provideFirebaseApp(() => initializeApp(environment.firebase.config)),
     provideFirestore(() => getFirestore()),
   ],
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,12 @@
-export const environment = {};
+export const environment = {
+  firebase: {
+    config: {
+      projectId: 'finanzas-vlv',
+      appId: '1:520471745398:web:b45d6db7915f7e2e24e057',
+      storageBucket: 'finanzas-vlv.appspot.com',
+      apiKey: 'AIzaSyAhmdd4YuHRtqqZuJ3oG0rcY0DNzDbAGyk',
+      authDomain: 'finanzas-vlv.firebaseapp.com',
+      messagingSenderId: '520471745398',
+    },
+  },
+};
